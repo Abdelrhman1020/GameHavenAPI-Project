418e3c3d54b4a610dd1a58a36a84a5b8a1afd15f
abdelrahman ali <aabdelrhman0555@gmail.com>
2025-06-04 18:45:45 +0300

    Fix rout connection between auth routes and app.js.

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import cors from 'cors';
+import authRoutes from './routes/auth.js';
+
+
+
 
 dotenv.config();
 const app = express();
-
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
+app.use('/auth', authRoutes);
 
 
 mongoose.connect(process.env.MONGODB_URI)
